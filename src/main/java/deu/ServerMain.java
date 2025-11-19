e65da425e4f4191aaa077d24df4ad9d223b0293a
hzxu02 <scq3799@naver.com>
2025-11-19 18:08:03 +0900

    feat : 서버메인 모니터링 번호 연결 및 편의성 추가

--- src/main/java/deu/ServerMain.java
+++ src/main/java/deu/ServerMain.java
@@ -13,44 +13,74 @@ import deu.moniter.LogManager;
 import deu.moniter.NormalState;
 
 public class ServerMain {
+    
+    //기본 값을 false(normal 모드)로 설정
+    private static boolean isDebugMode = false;
 
     public static void main(String[] args) {
         
-        LogManager.setState(new DebugState());
-        // 일반 모드: 로그가 하나도 안 뜸 (에러 날 때만 뜸)
-        //LogManager.setState(new NormalState());
-
+        
         Scanner scanner = new Scanner(System.in);
         ReservationRepository repo = ReservationRepository.getInstance();
 
         String backupPath = System.getProperty("user.dir")
                 + "/backup/reservations-backup.yaml";
-
+        
+         // 일반 모드: 로그가 하나도 안 뜸 (에러 날 때만 뜸)
+        LogManager.setState(new NormalState());
+        
+        boolean isServerRunning = false;
+        
+        while(!isServerRunning){
         System.out.println("=======================================================");
-        System.out.println("        강의실 예약 서버 (관리자 모드)");
+        System.out.println("                 강의실 예약 서버");
         System.out.println("=======================================================\n");
+        
+        // 현재 상태 표시
+        String currentStatus = isDebugMode ? "[상세(Debug)]" : "[일반(Normal)]";
 
         System.out.println("1) 예약 데이터 백업");
         System.out.println("2) 예약 데이터 복구");
         System.out.println("3) 서버 실행\n");
-        System.out.print("번호 입력: ");
+        System.out.println("4) 모니터링 모드 변경 " + currentStatus);
+        
+        System.out.print("\n번호 입력: ");
 
         String input = scanner.nextLine().trim();
         //엔터만 쳤을 경우 예외방지용 체크
-        int choice = input.isEmpty() ? 3 : Integer.parseInt(input);
+        int choice = input.isEmpty() ? 0 : Integer.parseInt(input);
         
-        //int choice = Integer.parseInt(scanner.nextLine().trim());
-
-        if (choice == 1) {
-            new BackupCommand(repo, backupPath).execute();
-            LogManager.log("관리자가 예약 데이터 백업을 실행하였습니다.");
-        } else if (choice == 2) {
-            new RestoreCommand(repo, backupPath).execute();
-            LogManager.log("관리자가 예약 데이터 복구를 실행하였습니다.");
+        switch (choice) {
+                case 1:
+                    new BackupCommand(repo, backupPath).execute();
+                    System.out.println(">> 백업 완료.");
+                    break;
+                case 2:
+                    new RestoreCommand(repo, backupPath).execute();
+                    System.out.println(">> 복구 완료.");
+                    break;
+                case 3:
+                    isServerRunning = true;
+                    break;
+                case 4:
+                    // 모드 변경 (토글)
+                    isDebugMode = !isDebugMode;
+                    
+                    if (isDebugMode) {
+                        LogManager.setState(new DebugState());
+                        System.out.println("\n>>> [설정] 모니터링 모드가 '상세(Debug)'로 변경되었습니다.");
+                    } else {
+                        LogManager.setState(new NormalState());
+                        System.out.println("\n>>> [설정] 모니터링 모드가 '일반(Normal)'로 변경되었습니다.");
+                    }
+                    break;
+                default:
+                    System.out.println(">> 잘못된 입력입니다.");
+            }
         }
 
         System.out.println("\n=======================================================");
-        LogManager.log("         서버 실행 중... 포트 번호: 9999");
+        System.out.println("         서버 실행 중... 포트 번호: 9999");
         System.out.println("=======================================================\n");
 
         try (ServerSocket serverSocket = new ServerSocket(9999)) {
@@ -62,7 +92,7 @@ public class ServerMain {
 
         } catch (Exception e) {
             //에러 발생시 로그 기록
-            LogManager.log("[FATAL ERROR] 서버 실행 중 문제 발생: " + e.getMessage());
+            System.err.println("[FATAL ERROR] 서버 실행 중 문제 발생: " + e.getMessage());
             e.printStackTrace();
         }
     }
